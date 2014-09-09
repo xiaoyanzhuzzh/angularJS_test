b3ef65766f59fdc5bd6110f8c2a7c92df2a1dca4
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2014-09-09 23:03:19 +0800

    add category add and modify function

--- app/scripts/controllers/categoryAdd.js
+++ app/scripts/controllers/categoryAdd.js
@@ -8,9 +8,25 @@ angular.module('myYoApp')
 
         $scope.categorys = Util.localStorage.getStorageItem('categorys');
 
-        $scope.deleteCurrentCategory = function (item) {
+        $scope.deleteCurrentCategory = function (category) {
 
           $scope.categorys = categoryService.deleteCategory($scope.categorys, category);
           $scope.items = categoryService.deleteItem($scope.items, category);
+        };
+
+        $scope.showSignal = false;
+
+        $scope.addCategory = function () {
+
+          $scope.showSignal = true;
+
+        };
+
+        $scope.addButton = function (newCategory) {
+
+          $scope.categorys.push(newCategory);
+          Util.localStorage.setStorageItem('categorys', $scope.categorys);
+
+          $scope.showSignal = false;
         };
     });
--- app/scripts/controllers/categoryModify.js
+++ app/scripts/controllers/categoryModify.js
@@ -6,11 +6,34 @@ angular.module('myYoApp')
 
         $scope.items = Util.localStorage.getStorageItem('items');
 
-        $scope.categorys = categoryService.getCategorys($scope.items);
+        //$scope.categorys = categoryService.getCategorys($scope.items);
+        $scope.categorys = Util.localStorage.getStorageItem('categorys');
 
         $scope.deleteCurrentCategory = function (category) {
 
           $scope.categorys = categoryService.deleteCategory($scope.categorys, category);
           $scope.items = categoryService.deleteItem($scope.items, category);
+        };
+
+        $scope.showModify = false;
+        $scope.modifyCurrentCategory = function (category) {
+
+          $scope.showModify = true;
+
+        };
+
+        $scope.changeCategory = function (category) {
+          console.log(category);
+          for (var i = 0; i < $scope.categorys.length; i++){
+            if(category === $scope.categorys[i]){
+              $scope.categorys[i] = category;
+              Util.localStorage.setStorageItem('categorys', $scope.categorys);
+            }
+          }
+        };
+
+        $scope.cancelModify = function () {
+
+          $scope.showModify = false;
         };
     });
