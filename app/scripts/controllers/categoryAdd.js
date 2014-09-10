3a1e7c9420fd64becc97effb5193736c283bf617
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2014-09-10 17:42:10 +0800

    change category interface

--- app/scripts/controllers/categoryAdd.js
+++ app/scripts/controllers/categoryAdd.js
@@ -5,28 +5,34 @@ angular.module('myYoApp')
     .controller('CategoryAddCtrl', function ($scope, categoryService) {
 
         $scope.items = Util.localStorage.getStorageItem('items');
-        
+
         $scope.categorys = Util.localStorage.getStorageItem('categorys');
 
         $scope.deleteCurrentCategory = function (category) {
 
-          $scope.categorys = categoryService.deleteCategory($scope.categorys, category);
-          $scope.items = categoryService.deleteItem($scope.items, category);
+            $scope.categorys = categoryService.deleteCategory($scope.categorys, category);
+            $scope.items = categoryService.deleteItem($scope.items, category);
         };
 
         $scope.showSignal = false;
 
-        $scope.addCategory = function () {
+        $scope.addCategoryButton = function () {
 
-          $scope.showSignal = true;
+            $scope.showSignal = true;
 
         };
 
-        $scope.addButton = function (newCategory) {
+        $scope.addNewCategoryButton = function (newCategory) {
 
-          $scope.categorys.push(newCategory);
-          Util.localStorage.setStorageItem('categorys', $scope.categorys);
+            $scope.categorys.push(newCategory);
+            Util.localStorage.setStorageItem('categorys', $scope.categorys);
+
+            $scope.showSignal = false;
+        };
+
+        $scope.cancelButton = function () {
 
           $scope.showSignal = false;
         };
+
     });
