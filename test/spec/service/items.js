21716605422e75e606e53eb48029096b8ba92e91
zzh <xiaoyanzhuzzh@sina.com>
2014-09-17 13:56:46 +0800

    complete itemService test

--- test/spec/service/items.js
+++ test/spec/service/items.js
@@ -23,4 +23,22 @@ describe('itemsService', function () {
       expect(items[1].name).toEqual('雪碧');
       expect(items[2].category).toEqual('水果');
     });
+
+    it ('should have get function', function(){
+
+
+      spyOn(localStorageService, 'get');
+      itemsService.get('items');
+
+      expect(localStorageService.get.calls.length).toBe(1);
+    });
+
+    it ('should load all the same items', function(){
+
+      var cartCount = 9;
+      spyOn(localStorageService, 'set');
+      itemsService.set('cartCount', cartCount);
+
+      expect(localStorageService.set.calls.length).toBe(1);
+    });
 });
