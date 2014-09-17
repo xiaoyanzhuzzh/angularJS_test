6c946e4ec4613d93a899f8740c4664c09ef51fef
zzh <xiaoyanzhuzzh@sina.com>
2014-09-17 20:40:43 +0800

    complete index test codes

--- test/spec/controllers/index.js
+++ test/spec/controllers/index.js
@@ -1,7 +1,7 @@
 'use strict';
-describe('addCartCountCtrl', function () {
+describe('indexCtrl', function () {
 
-  var $scope, itemsService, createController;
+  var $scope, $rootScope, itemsService, createController;
 
   beforeEach(function () {
        module('myYoApp');
@@ -9,13 +9,14 @@ describe('addCartCountCtrl', function () {
        inject(function ($injector) {
 
            $scope = $injector.get('$rootScope').$new();
+           $rootScope = $injector.get('$rootScope');
            itemsService = $injector.get('itemsService');
 
            var $controller = $injector.get('$controller');
 
            createController = function () {
 
-             return $controller ('addCartCountCtrl', {
+             return $controller ('indexCtrl', {
                   $scope: $scope,
                   itemsService: itemsService
              });
@@ -32,32 +33,91 @@ describe('addCartCountCtrl', function () {
     expect(itemsService.get.calls.length).toBe(1);
   });
 
-  describe('addCartCount',function () {
+  it('should to-parent-cartCount can do',function(){
 
-    beforeEach(function () {
+    spyOn(itemsService, 'get').andReturn(undefined);
+    spyOn(itemsService, 'set');
+    createController();
 
-      spyOn(itemsService, 'set');
-    });
+    $scope.$digest();
+    $rootScope.$broadcast('to-parent-cartCount');
+    $scope.$digest();
 
-    it ('should make cartCount add by 1', function () {
-      spyOn(itemsService, 'get').andReturn(31);
-      createController();
-      $scope.addCartCount();
+    expect($scope.cartCount).toBe(1);
+    expect(itemsService.get.calls.length).toBe(1);
+    expect(itemsService.set.calls.length).toBe(1);
+  });
 
-      expect($scope.cartCount).toEqual(32);
-      expect(itemsService.get.calls.length).toBe(1);
-      expect(itemsService.set.calls.length).toBe(1);
-    });
 
-    it ('should make cartItems be 0', function () {
+  it('should to-parent-cartCount can do',function(){
 
-      spyOn(itemsService, 'get').andReturn(undefined);
-      createController();
-      $scope.addCartCount();
+    spyOn(itemsService, 'get').andReturn(4);
+    spyOn(itemsService, 'set');
+    createController();
 
-      expect($scope.cartCount).toEqual(1);
-      expect(itemsService.get.calls.length).toBe(1);
-      expect(itemsService.set.calls.length).toBe(1);
-    });
+    $scope.$digest();
+    $rootScope.$broadcast('to-parent-cartCount');
+    $scope.$digest();
+
+    expect($scope.cartCount).toBe(5);
+    expect(itemsService.get.calls.length).toBe(1);
+    expect(itemsService.set.calls.length).toBe(1);
+  });
+
+  it('should to-parent-homeActive can do',function(){
+
+    createController();
+
+    $scope.$digest();
+    $rootScope.$broadcast('to-parent-homeActive');
+    $scope.$digest();
+
+    expect($scope.homeActive).toBe(true);
+    expect($scope.itemsListActive).toBe(false);
+    expect($scope.cartItemsListActive).toBe(false);
+    expect($scope.cartPayListActive).toBe(false);
+  });
+
+  it('should to-parent-itemsListActive can do',function(){
+
+    createController();
+
+    $scope.$digest();
+    $rootScope.$broadcast('to-parent-itemsListActive');
+    $scope.$digest();
+
+    expect($scope.homeActive).toBe(false);
+    expect($scope.itemsListActive).toBe(true);
+    expect($scope.cartItemsListActive).toBe(false);
+    expect($scope.cartPayListActive).toBe(false);
+  });
+
+  it('should to-parent-cartItemsListActive can do',function(){
+
+    createController();
+
+    $scope.$digest();
+    $rootScope.$broadcast('to-parent-cartItemsListActive');
+    $scope.$digest();
+
+    expect($scope.homeActive).toBe(false);
+    expect($scope.itemsListActive).toBe(false);
+    expect($scope.cartItemsListActive).toBe(true);
+    expect($scope.cartPayListActive).toBe(false);
+  });
+
+
+  it('should to-parent-cartPayListActive can do',function(){
+
+    createController();
+
+    $scope.$digest();
+    $rootScope.$broadcast('to-parent-cartPayListActive');
+    $scope.$digest();
+
+    expect($scope.homeActive).toBe(false);
+    expect($scope.itemsListActive).toBe(false);
+    expect($scope.cartItemsListActive).toBe(false);
+    expect($scope.cartPayListActive).toBe(true);
   });
 });
